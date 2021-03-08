4346ff2bc6c9143269eafd770397fc63dc6e7de7
hongseonghun-web <hh4518@gmail.com>
2021-03-08 23:49:54 +0900

    날짜 선택 함수 수정

--- js/term.js
+++ js/term.js
@@ -1,35 +1,12 @@
 function term(startDate, endDate, calendar) {
     const calendarList = document.querySelectorAll('.calendar__list');
-
-    const year = parseInt(document.querySelector('.select-year').innerText);
-    const month = parseInt(document.querySelector('.select-month').innerText);
-    const yearMonth = `${year}/${month}`;
-
-    const s_split = startDate.split('/');
-    const e_split = endDate.split('/');
-
-    const s_year = parseInt(s_split[0]);
-    const s_month = parseInt(s_split[1]);
-    const e_year = parseInt(e_split[0]);
-    const e_month = parseInt(e_split[1]);
-
-    const s_yearMonth = `${s_year}/${s_month}`;
-    const e_yearMonth = `${e_year}/${e_month}`;
-    const s_date = parseInt(s_split[2]);
-    const e_date = parseInt(e_split[2]);
+    const year = document.querySelector('.select-year').innerText;
+    const month = document.querySelector('.select-month').innerText;
 
     const selectDate = [].filter.call(calendarList, list => {
-        if (yearMonth === s_yearMonth && yearMonth === e_yearMonth) {
-            return !list.classList.contains('not-current-month') && list.querySelector('.calendar__date').innerText >= s_date && list.querySelector('.calendar__date').innerText <= e_date;
-        } else if (yearMonth === s_yearMonth && yearMonth !== e_yearMonth) {
-            return (!list.classList.contains('not-current-month') && list.querySelector('.calendar__date').innerText >= s_date) || 
-            (list.classList.contains('not-current-month') && list.querySelector('.calendar__date').innerText <= e_date);
-        } else if (yearMonth !== s_yearMonth && yearMonth === e_yearMonth) {
-            return (list.classList.contains('not-current-month') && list.querySelector('.calendar__date').innerText >= s_date) || 
-            (!list.classList.contains('not-current-month') && list.querySelector('.calendar__date').innerText <= e_date);
-        } else if (year === s_year && year === e_year && month > s_month && month < e_month){
-            return list;
-        }
+        const select = `${year} / ${month} / ${list.querySelector('.calendar__date').innerText}`;
+
+        return startDate <= select && endDate >= select;
     });
     
     if (calendarList[0].parentNode.classList.contains('calendar-task')) {
