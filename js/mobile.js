98695a44a5c779def7686ab14065886ef6818c89
unknown <hh4518@gmail.com>
2021-04-05 09:09:19 +0900

    모바일 조작 기능 수정

--- js/mobile.js
+++ js/mobile.js
@@ -1,26 +1,9 @@
-_cal.createCalendar('mobile');
+_cal.createObject('mobile');
 
 _cal.mobile.moveX;
-_cal.mobile.moveY;
 
-_cal.mobile.minuteSetting = (e, target) => {
-    const movement = e.changedTouches[0].clientY - _cal.mobile.moveY;
-
-    _cal.timeSet.preventScroll(e);
-    _cal.timeSet.timeSelection(target, e, movement);
-    _cal.timeSet.hoursActivation();
-}
-
-_cal.mobile.hourSetting = (e, target) => {
-    const movement = e.changedTouches[0].clientY - _cal.mobile.moveY;
-
-    _cal.timeSet.preventScroll(e);
-    _cal.timeSet.timeSelection(target, e, movement);
-    _cal.timeSet.minutesActivation(target);
-}
-
-_cal.mobile.createCalendar = e => {
-    const movement = e.changedTouches[0].clientX - _cal.mobile.moveX;
+_cal.mobile.createCalendar = event => {
+    const movement = event.changedTouches[0].clientX - _cal.mobile.moveX;
 
     if (movement > 0) {
         _cal.calendarControl.nextMonth();
@@ -32,30 +15,11 @@ _cal.mobile.createCalendar = e => {
 
 // CALENDAR CONTROL
 // TOUCH START
-_cal.calendarArea.addEventListener('touchstart', function(e) {
-    _cal.mobile.moveX = e.touches[0].clientX;
+_cal.calendarArea.addEventListener('touchstart', function(event) {
+    _cal.mobile.moveX = event.touches[0].clientX;
 });
 
 // TOUCH END
-_cal.calendarArea.addEventListener('touchend', function(e) {
-    _cal.mobile.createCalendar(e);
-});
-
-// TIME SETTING CONTROL
-// TOUCH START
-_cal.addTaskVar.hours.addEventListener('touchstart', function(e) {
-    _cal.mobile.moveY = e.touches[0].clientY;
-});
-
-_cal.addTaskVar.minutes.addEventListener('touchstart', function(e) {
-    _cal.mobile.moveY = e.touches[0].clientY;
-});
-
-// TOUCHEND
-_cal.addTaskVar.hours.addEventListener('touchend', function(e) {
-    _cal.mobile.hourSetting(e, this);
-});
-
-_cal.addTaskVar.minutes.addEventListener('touchend', function(e) {
-    _cal.mobile.minuteSetting(e, this);
+_cal.calendarArea.addEventListener('touchend', function(event) {
+    _cal.mobile.createCalendar(event);
 });
---
+++ js/task/time-setting-mobile.js
@@ -0,0 +1,36 @@
+_cal.mobile.moveY;
+
+_cal.mobile.minuteSetting = (event, target) => {
+    const movement = event.changedTouches[0].clientY - _cal.mobile.moveY;
+
+    _cal.timeSet.preventScroll(event);
+    _cal.timeSet.timeSelection(target, event, movement);
+    _cal.timeSet.hoursActivation();
+}
+
+_cal.mobile.hourSetting = (event, target) => {
+    const movement = event.changedTouches[0].clientY - _cal.mobile.moveY;
+
+    _cal.timeSet.preventScroll(event);
+    _cal.timeSet.timeSelection(target, event, movement);
+    _cal.timeSet.minutesActivation(target);
+}
+
+// TIME SETTING CONTROL
+// TOUCH START
+_cal.addTaskVar.hours.addEventListener('touchstart', function(event) {
+    _cal.mobile.moveY = event.touches[0].clientY;
+});
+
+_cal.addTaskVar.minutes.addEventListener('touchstart', function(event) {
+    _cal.mobile.moveY = event.touches[0].clientY;
+});
+
+// TOUCHEND
+_cal.addTaskVar.hours.addEventListener('touchend', function(event) {
+    _cal.mobile.hourSetting(event, this);
+});
+
+_cal.addTaskVar.minutes.addEventListener('touchend', function(event) {
+    _cal.mobile.minuteSetting(event, this);
+});
